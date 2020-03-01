95691d74c04043776223398756f4e100e81064c8
Renzo Matonti <matontirenzo@gmail.com>
2020-03-01 17:31:25 -0300

    ReadFile
    ReadFile on .Net C#

---
+++ ReadFile/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReadFile {
+    class Program {
+        static void Main(string[] args)
+        {
+            int cantidad = 0;
+            string linea;
+            string filename = "C:\\test.txt";
+
+            // Read the file and display it line by line
+
+            System.IO.StreamReader file = new System.IO.StreamReader(filename);
+
+            while ((linea = file.ReadLine()) != null) {
+
+                Console.WriteLine(cantidad + 1 + ". " + linea);
+                cantidad++;
+            }
+
+            file.Close();
+            Console.WriteLine();
+        }
+    }
+}
